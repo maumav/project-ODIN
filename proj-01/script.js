4c2b19be713c386af5126abe267315e77e1bef00
mavmav <maumandac@gmail.com>
2026-04-22 00:56:43 +1200

    Add changes

--- proj-01/script.js
+++ proj-01/script.js
@@ -1,5 +1,8 @@
 console.log("Hello JS 01")
 
+humanScore = 0;
+computerScore = 0; 
+
 function getComputerChoice() {
     let choice = ['rock', 'paper', 'scissors'] 
     return choice[Math.floor(Math.random() * choice.length)]
@@ -7,3 +10,7 @@ function getComputerChoice() {
 function getHumanChoice() {
     let input = prompt("Enter choice here: ")
 }console.log(getHumanChoice)
+
+function playRound(humanChoice, computerChoice) {
+    let getHumanChoice = prompt().toLowerCase()
+}
