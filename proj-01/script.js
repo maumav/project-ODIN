d83045b2f5288610beab087d5edda50a1b29062d
mavmav <maumandac@gmail.com>
2026-04-22 00:41:36 +1200

    Start rock paper scissor project

--- proj-01/script.js
+++ proj-01/script.js
@@ -1,1 +1,9 @@
 console.log("Hello JS 01")
+
+function getComputerChoice() {
+    let choice = ['rock', 'paper', 'scissors'] 
+    return choice[Math.floor(Math.random() * choice.length)]
+}
+function getHumanChoice() {
+    let input = prompt("Enter choice here: ")
+}console.log(getHumanChoice)
