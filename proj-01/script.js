7304c931d214415cf78c15dbfda06f593a159f8e
mavmav <maumandac@gmail.com>
2026-04-23 17:07:52 +1200

    Figure out the playRound function

--- proj-01/script.js
+++ proj-01/script.js
@@ -1,16 +1,44 @@
 console.log("Hello JS 01")
 
-humanScore = 0;
-computerScore = 0; 
+let humanScore = 0;
+let computerScore = 0; 
 
 function getComputerChoice() {
     let choice = ['rock', 'paper', 'scissors'] 
-    return choice[Math.floor(Math.random() * choice.length)]
+    return choice[Math.floor(Math.random() * choice.length)];
+    
 }
+
 function getHumanChoice() {
-    let input = prompt("Enter choice here: ")
-}console.log(getHumanChoice)
+    let input = prompt("Enter choice here: ").toLowerCase()
+       return input;   
+} 
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
 
 function playRound(humanChoice, computerChoice) {
-    let getHumanChoice = prompt().toLowerCase()
+
+    if (humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log("Human wins!")
+       } else if (
+       (humanChoice === 'scissors' && computerChoice === 'paper') ||
+       (humanChoice === 'rock' && computerChoice === 'scissors') 
+       humanScore++;
+        )
+        console.log("Human wins!! ")
+    
+        } {
+          console.log("Human wins!!!")
+        } 
+       
+        console.log("You chooses " + humanChoice + " and " + "computer chooses " + computerChoice)
 }
+playRound(humanSelection, computerSelection)
+
+// while (getComputerChoice === 'rock') {
+//     console.log("Computer chooses ROCK.")
+//    } else if (getComputerChoice === 'paper') {
+//     console.log("Computer chooses PAPER.")
+//    }
+
